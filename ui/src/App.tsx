66bc35b45c8ca5cb2aa9fa707ef6a14e0687e93e
Justin Chu <justinchu24.com>
2023-09-20 15:46:39 -0400

    pulled buildable repo. attempting to add iframe to front-end

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -15,38 +15,31 @@ export function App() {
   const [response, setResponse] = React.useState<string>();
   const ddClient = useDockerDesktopClient();
 
-  const fetchAndDisplayResponse = async () => {
-    const result = await ddClient.extension.vm?.service?.get('/hello');
-    setResponse(JSON.stringify(result));
-  };
-
   return (
     <>
-      <Typography variant="h3">Docker extension demo</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+      <Typography variant='h3'>Docker extension demo</Typography>
+      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
         This is a basic page rendered with MUI, using Docker's theme. Read the
         MUI documentation to learn more. Using MUI in a conventional way and
         avoiding custom styling will help make sure your extension continues to
         look great as Docker's theme evolves.
       </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Pressing the below button will trigger a request to the backend. Its
-        response will appear in the textarea.
+      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
+        Click this button to test IFrame:
       </Typography>
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
-          Call backend
+      <Stack direction='row' alignItems='start' spacing={2} sx={{ mt: 4 }}>
+        <Button variant='contained' /*onClick={fetchAndDisplayResponse}*/>
+          IFrame
         </Button>
 
-        <TextField
-          label="Backend response"
-          sx={{ width: 480 }}
-          disabled
-          multiline
-          variant="outlined"
-          minRows={5}
-          value={response ?? ''}
-        />
+        <iframe
+          src='https://www.sdrive.app/embed/1ptBQD'
+          width='640px'
+          height='320px'
+          id=''
+          className='test'
+
+        ></iframe>
       </Stack>
     </>
   );
