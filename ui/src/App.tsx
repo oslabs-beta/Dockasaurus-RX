810ab439749f2a67026951b6f453d39281333f80
CD Robinson <cd@christian-daniel.com>
2023-09-23 08:19:24 -0700

    responsive chakra front-end with suggestions lorem panel mock up

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { Text } from '@chakra-ui/react';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
-import { Stack, TextField, Typography } from '@mui/material';
+import { Stack, HStack, VStack, StackDivider } from '@chakra-ui/react';
+import { Button, ButtonGroup, Box } from '@chakra-ui/react';
+import { Textarea } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
+import { Accordion, AccordionItem, AccordionIcon, AccordionButton, AccordionPanel } from '@chakra-ui/react';
+import '../../ui/css/style.css';
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -22,32 +29,130 @@ export function App() {
 
   return (
     <>
-      <Typography variant='h3'>Docker extension demo</Typography>
-      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
+      {/* <Text fontSize='3xl'>Docker extension demo</Text>
+      <Text fontSize='sm'>
         This is a basic page rendered with MUI, using Docker's theme. Read the
         MUI documentation to learn more. Using MUI in a conventional way and
         avoiding custom styling will help make sure your extension continues to
         look great as Docker's theme evolves.
-      </Typography>
-      <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
+      </Text>
+      <Text fontSize='sm'>
         Pressing the below button will trigger a request to the backend. Its
         response will appear in the textarea.
-      </Typography>
-      <Stack direction='row' alignItems='start' spacing={2} sx={{ mt: 4 }}>
-        <Button variant='contained' onClick={fetchAndDisplayResponse}>
+      </Text>
+      <VStack spacing='24px'>
+        <Button colorScheme='blackAlpha' variant='outline' onClick={fetchAndDisplayResponse}>
           Call backend
         </Button>
 
-        <TextField
-          label='Backend response'
-          sx={{ width: 480 }}
-          disabled
-          multiline
-          variant='outlined'
-          minRows={5}
+        <Textarea
+          placeholder='Backend response'
+          size='md'
           value={response ?? ''}
         />
-      </Stack>
+      </VStack>
+      <VStack
+  divider={<StackDivider borderColor='gray.200' />}
+  spacing={4}
+  align='stretch'
+>
+  <Box h='40px' bg='yellow.200'>
+    1
+  </Box>
+  <Box h='40px' bg='tomato'>
+    2
+  </Box>
+  <Box h='40px' bg='pink.100'>
+    3
+  </Box>
+</VStack> */}
+      {/* <Grid
+  h='700'
+  templateRows='repeat(3, 1fr)'
+  templateColumns='repeat(5, 1fr)'
+  gap={4}
+>
+  <GridItem rowSpan={3} colSpan={1} bg='tomato' />
+  <GridItem colSpan={1} bg='orange.300' />
+  <GridItem colSpan={1} bg='papayawhip' />
+  <GridItem colSpan={1} bg='pink.300' />
+  <GridItem colSpan={1} bg='yellow.300' />
+  <GridItem colSpan={2} bg='green.200' />
+  <GridItem colSpan={2} bg='blue.300' />
+  <GridItem colSpan={2} bg='gray.200' />  position='relative'
+  <GridItem colSpan={2} bg='purple.300' /> bg='tomato' p='4' color='white' axis='both'
+</Grid> */}
+      <Box bg='gray.200'>
+        <Grid
+          templateRows='repeat(2, 2fr)'
+          templateColumns='repeat(2, 2fr)'
+          gap={8}
+          p='10'
+          h='100vh'
+          color='blackAlpha'>
+          <GridItem colSpan={2} bg='white' boxShadow='md' p='6' rounded='md'>
+            Grafana Dashboard:
+          </GridItem>
+          <GridItem colSpan={1} bg='white' boxShadow='md' p='6' rounded='md'>
+            Container Selection Panel:
+          </GridItem>
+          <GridItem colSpan={1} bg='white' boxShadow='md' p='6' rounded='md'>
+            Suggestions Panel:
+            <Accordion>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton _expanded={{ bg: 'green.500', color: 'white' }}>
+                    <Box as='span' flex='1' textAlign='left'>
+                      Resource Optimization 1
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton _expanded={{ bg: 'green.400', color: 'white' }}>
+                    <Box as='span' flex='1' textAlign='left'>
+                    Resource Optimization 2
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton _expanded={{ bg: 'green.300', color: 'white' }}>
+                    <Box as='span' flex='1' textAlign='left'>
+                    Resource Optimization 3
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </GridItem>
+        </Grid>
+      </Box>
     </>
   );
 }
