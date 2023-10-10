69d16d3283110bbd9c8f5d082a7ad385b177806c
CD Robinson <cd@christian-daniel.com>
2023-10-10 01:09:38 -0700

    modularized useMenuState, StyledMenu, GraphData and responsive graphs

--- ui/components/GraphData.tsx
+++ ui/components/GraphData.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import StyledMenu from './StyledMenu';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import {
-  Button,
-  MenuItem,
-  Divider
-} from '@mui/material';
+import { Box, Button, MenuItem, Divider } from '@mui/material';
 import { useMenuState } from '../hooks/useMenuState';
+import '../../ui/css/style.css';
 
 const GrafanaData = () => {
   const {
@@ -25,64 +22,69 @@ const GrafanaData = () => {
   } = useMenuState();
 
   return (
-    <div>
-      <iframe
-        id='iframe1'
-        src={`http://localhost:40001/d-solo/b6cb1312-2136-4c9b-b59a-e45ff2fce572/container-metrics?orgId=1&to=now&theme=dark&panelId=2&from=now-${view}`}
-        height='300'
-        style={{ width: '100%', border: '0'}}
-      />
-      <div>
-        <Button
-          id='time-button'
-          aria-controls={open ? 'customized-menu' : undefined}
-          aria-haspopup='true'
-          aria-expanded={open ? 'true' : undefined}
-          disableElevation
-          onClick={handleClick}
-          endIcon={<KeyboardArrowDownIcon />}
-          variant='text'
-          sx={{
-            textTransform: 'uppercase',
-            fonSize: '0.95em',
-            borderRadius: '20px',
-            padding: '0.35rem',
-            margin: '3px',
-          }}>
-          View
-        </Button>
-        <StyledMenu
-          id='demo-customized-menu'
-          MenuListProps={{
-            'aria-labelledby': 'demo-customized-button',
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}>
-          <MenuItem value={'5m'} onClick={handleViewClick} disableRipple>
-            Last 5 minutes
-          </MenuItem>
-          <Divider sx={{ my: 0.5 }} />
-          <MenuItem value={'15m'} onClick={handleViewClick} disableRipple>
-            Last 15 minutes
-          </MenuItem>
-          <Divider sx={{ my: 0.5 }} />
-          <MenuItem value={'30m'} onClick={handleViewClick} disableRipple>
-            Last 30 minutes
-          </MenuItem>
-          <Divider sx={{ my: 0.5 }} />
-          <MenuItem value={'1d'} onClick={handleViewClick} disableRipple>
-            Last 24 hours
-          </MenuItem>
-        </StyledMenu>
-      </div>
-      <iframe
-        id='iframe2'
-        src={`http://localhost:40001/d-solo/b6cb1312-2136-4c9b-b59a-e45ff2fce572/container-metrics?orgId=1&to=now&theme=dark&panelId=1&from=now-${view}`}
-        height='300'
-        style={{ width: '100%', border: '0'}}
-      />
-    </div>
+    <>
+      <Box
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-evenly',
+          height: '90%'
+        }}>
+        <iframe
+          className='framed'
+          id='iframe1'
+          src={`http://localhost:40001/d-solo/b6cb1312-2136-4c9b-b59a-e45ff2fce572/container-metrics?orgId=1&to=now&theme=dark&panelId=2&from=now-${view}`}
+        />
+
+        <iframe
+          className='framed'
+          id='iframe2'
+          src={`http://localhost:40001/d-solo/b6cb1312-2136-4c9b-b59a-e45ff2fce572/container-metrics?orgId=1&to=now&theme=dark&panelId=1&from=now-${view}`}
+        />
+      </Box>
+      <Button
+        id='time-button'
+        aria-controls={open ? 'customized-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        variant='text'
+        sx={{
+          textTransform: 'uppercase',
+          fonSize: '0.95em',
+          borderRadius: '30px',
+          padding: '0.35rem',
+          margin: '3px',
+        }}>
+        Select Time Period
+      </Button>
+      <StyledMenu
+        id='demo-customized-menu'
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}>
+        <MenuItem value={'5m'} onClick={handleViewClick} disableRipple>
+          5 minutes
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem value={'15m'} onClick={handleViewClick} disableRipple>
+          15 minutes
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem value={'30m'} onClick={handleViewClick} disableRipple>
+          30 minutes
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem value={'1d'} onClick={handleViewClick} disableRipple>
+          Last 24 hours
+        </MenuItem>
+      </StyledMenu>
+    </>
   );
 };
 
